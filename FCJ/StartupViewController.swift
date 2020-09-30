4bddbd5929552a73db767434e98935330cc7271e
Marko Nikolic <thenikolic@mail.com>
2020-09-30 15:33:14 +0200

    projects files
    init project commit

---
+++ FCJ/StartupViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StartupViewController.swift
+//  FCJ
+//
+
+import UIKit
+
+class StartupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
+    }
+}
